72234bf1e908470ed6451c68fe136c7e0b938dae
mmcinnes123 <m.mcinnnes1.22@abdn.ac.uk>
2024-03-09 19:01:46 +0000

    Added function for calculating orientation error between real and perfect IMU + plots.

--- IMU_preprocess.py
+++ IMU_preprocess.py
@@ -14,6 +14,7 @@ input_file_Perfect = "26thFeb_Greg - Report3 - Cluster_Quats.txt"     # Name of 
 input_file_Real = "26thFeb_Greg - Report2 - IMU_Quats.txt"     # Name of the txt file with real IMU data
 cal_pose_time_dict = {"Cal_pose_1a": 10, "Cal_pose_2a": 13, "Cal_pose_2b": 15}  # List of pose times for calibration
 sample_rate = 100
+static_time = 10    # Input first known static time to use as reference for changing orientation error
 
 # Required Files in Folder
 template_file = "APDM_template_4S.csv"
@@ -34,12 +35,13 @@ osim.Logger.setLevelString("Off")
 
 def write_movements_and_calibration_stos(file_path, cal_pose_time_dict, IMU_type):
 
-    # Read data in from file
+    # Read data from TMM .txt report
     IMU1_df, IMU2_df, IMU3_df = read_data_frame_from_file(file_path)
 
-    # Write transformed IMU quaternions to .sto file (write to APDM .csv first, then convert)
+    # Write data to APDM format .csv
     file_tag = "APDM_Quats_" + IMU_type
     write_to_APDM(IMU1_df, IMU2_df, IMU3_df, IMU3_df, template_file, results_dir, file_tag)
+    # Write data to .sto using OpenSim APDM converter tool
     APDM_2_sto_Converter(APDM_settings_file, input_file_name=results_dir + "\\" + file_tag + ".csv",
                          output_file_name=results_dir + "\\" + file_tag + ".sto")
 
@@ -48,18 +50,66 @@ def write_movements_and_calibration_stos(file_path, cal_pose_time_dict, IMU_type
 
         cal_pose_time = cal_pose_time_dict[pose_name]
 
-        # Extract row based on moment of calibration pose
+        # Extract one row based on time of calibration pose
         IMU1_cal_df = extract_cal_row(IMU1_df, cal_pose_time, sample_rate)
         IMU2_cal_df = extract_cal_row(IMU2_df, cal_pose_time, sample_rate)
         IMU3_cal_df = extract_cal_row(IMU3_df, cal_pose_time, sample_rate)
 
-        # Write calibration quaternions to .sto file (write to APDM .csv first, then convert)
+        # Write data to APDM format .csv
         file_tag = "APDM_Quats_" + IMU_type + "_" + str(cal_pose_time) + "s_"
         write_to_APDM(IMU1_cal_df, IMU2_cal_df, IMU3_cal_df, IMU3_cal_df, template_file, results_dir, file_tag)
+        # Write data to .sto using OpenSim APDM converter tool
         APDM_2_sto_Converter(APDM_settings_file, input_file_name=results_dir + "\\" + file_tag + ".csv",
                              output_file_name=results_dir + "\\" + file_tag + ".sto")
 
+    return IMU1_df, IMU2_df, IMU3_df
+
+IMU1_df_Perfect, IMU2_df_Perfect, IMU3_df_Perfect = write_movements_and_calibration_stos(file_path_Perfect, cal_pose_time_dict, IMU_type="Perfect")
+IMU1_df_Real, IMU2_df_Real, IMU3_df_Real = write_movements_and_calibration_stos(file_path_Real, cal_pose_time_dict, IMU_type="Real")
 
 
-write_movements_and_calibration_stos(file_path_Perfect, cal_pose_time_dict, IMU_type="Perfect")
-write_movements_and_calibration_stos(file_path_Real, cal_pose_time_dict, IMU_type="Real")
+
+    # TODO: Tidy below
+
+# Calculate orientation accuracy of real IMUs compared with perfect IMUs
+    # Comparing CHANGE in orientation, since exact orientations will be different due to different global frames
+
+def compare_oris_real_vs_perfect(IMU_df_Real, IMU_df_Perfect):
+
+    # Remove any rows (from both real and perfect dfs) where nan values appear (due to cluster occlusion)
+    IMU_Perfect = IMU_df_Perfect[IMU_df_Perfect.notna().any(axis=1) & IMU_df_Real.notna().any(axis=1)]
+    IMU_Real = IMU_df_Real[IMU_df_Perfect.notna().any(axis=1) & IMU_df_Real.notna().any(axis=1)]
+
+
+    # Get scipy format orientations, ready for comparison
+    IMU_Perfect_R = R.from_quat(IMU_Perfect.to_numpy()[:, [1, 2, 3, 0]])
+    IMU_Real_R = R.from_quat(IMU_Real.to_numpy()[:, [1, 2, 3, 0]])
+
+    # Get CHANGE in orientations, relative to orientation at time = ? (Needs to be static - add a variable to quick settings)
+    static_time_index = static_time*sample_rate
+    ori_change_Perfect_R = IMU_Perfect_R[static_time_index].inv() * IMU_Perfect_R
+    ori_change_Real_R = IMU_Real_R[static_time_index].inv() * IMU_Real_R
+
+    # Compare real vs perfect change in orientations
+    ori_diff = ori_change_Perfect_R.inv() * ori_change_Real_R
+    single_angle_diff = ori_diff.magnitude() * 180 / np.pi
+
+    return single_angle_diff
+
+
+IMU1_single_angle_diff = compare_oris_real_vs_perfect(IMU1_df_Real, IMU1_df_Perfect)
+IMU2_single_angle_diff = compare_oris_real_vs_perfect(IMU2_df_Real, IMU2_df_Perfect)
+IMU3_single_angle_diff = compare_oris_real_vs_perfect(IMU3_df_Real, IMU3_df_Perfect)
+
+
+
+# Calculate RMSE and plot time-varying error
+IMU1_single_angle_RMSE, IMU2_single_angle_RMSE, IMU3_single_angle_RMSE = plot_compare_real_vs_perfect(IMU1_single_angle_diff, IMU2_single_angle_diff, IMU3_single_angle_diff, parent_dir)
+
+# Write final RMSE values to a csv
+final_RMSE_values_df = pd.DataFrame.from_dict(
+    {"Thorax IMU orientation error:": IMU1_single_angle_RMSE,
+     "Humerus IMU orientation error:": IMU2_single_angle_RMSE,
+     "Forearm IMU orientation error:": IMU3_single_angle_RMSE,}, orient='index')
+final_RMSE_values_df.to_csv(parent_dir + "\\" + "Real_vs_Perfect_Ori_Errors.csv",
+                            mode='w', encoding='utf-8', na_rep='nan')
--- functions.py
+++ functions.py
@@ -967,3 +967,96 @@ def plot_vector_HT_angles(thorax_OMC, humerus_OMC, thorax_IMU, humerus_IMU,
     fig.savefig(figure_results_dir + r"\HT_Vectors.png")
 
     return RMSE_angle1, RMSE_angle2, RMSE_angle3
+
+
+def plot_compare_real_vs_perfect(IMU1_diff, IMU2_diff, IMU3_diff, figure_results_dir):
+
+    label1 = "Thorax IMU orientation error"
+    label2 = "Humerus IMU orientation error"
+    label3 = "Forearm IMU orientation error"
+
+    # Calculate RMSE
+    RMSE_angle1 = (sum(np.square(IMU1_diff)) / len(IMU1_diff)) ** 0.5
+    RMSE_angle2 = (sum(np.square(IMU2_diff)) / len(IMU2_diff)) ** 0.5
+    RMSE_angle3 = (sum(np.square(IMU3_diff)) / len(IMU3_diff)) ** 0.5
+    max_error_angle1 = np.amax(IMU1_diff)
+    max_error_angle2 = np.amax(IMU2_diff)
+    max_error_angle3 = np.amax(IMU3_diff)
+
+    # Create figure with three subplots
+    fig, axs = plt.subplots(3, 1, figsize=(14, 9))
+
+    # Plot error graphs
+    time1 = np.array(range(len(IMU1_diff)))*0.01
+    time2 = np.array(range(len(IMU2_diff)))*0.01
+    time3 = np.array(range(len(IMU3_diff)))*0.01
+
+    start_time = time1[0]
+    end_time = time1[-1]
+
+    axs[0].scatter(time1, IMU1_diff, s=0.4)
+    axs[1].scatter(time2, IMU2_diff, s=0.4)
+    axs[2].scatter(time3, IMU3_diff, s=0.4)
+
+    axs[0].set_title(label1)
+    axs[1].set_title(label2)
+    axs[2].set_title(label3)
+
+    # Plot RMSE error lines and text
+    axs[0].axhline(y=RMSE_angle1, linewidth=2, c="red")
+    axs[0].text(time1[-1] + 0.1 * (end_time - start_time), RMSE_angle1, "RMSE = " + str(round(RMSE_angle1, 1)) + " deg")
+    axs[1].axhline(y=RMSE_angle2, linewidth=2, c="red")
+    axs[1].text(time1[-1] + 0.1 * (end_time - start_time), RMSE_angle2, "RMSE = " + str(round(RMSE_angle2, 1)) + " deg")
+    axs[2].axhline(y=RMSE_angle3, linewidth=2, c="red")
+    axs[2].text(time1[-1] + 0.1 * (end_time - start_time), RMSE_angle3, "RMSE = " + str(round(RMSE_angle3, 1)) + " deg")
+
+
+    # Functions to define placement of max error annotation
+    def y_max_line_placement(max_error):
+        if max_error > 40:
+            line_placement = 40
+        else:
+            line_placement = max_error
+        return line_placement
+
+
+    def y_max_text_placement(max_error, RMSE):
+        if max_error > 40:
+            text_placement = 40
+        elif max_error < (RMSE + 3):
+            text_placement = RMSE * 1.1
+        else:
+            text_placement = max_error
+        return text_placement
+
+
+    # Plot max error lines
+    y_max_line_placement_1 = y_max_line_placement(max_error_angle1)
+    y_max_line_placement_2 = y_max_line_placement(max_error_angle2)
+    y_max_line_placement_3 = y_max_line_placement(max_error_angle3)
+    axs[0].axhline(y=y_max_line_placement_1, linewidth=1, c="red")
+    axs[1].axhline(y=y_max_line_placement_2, linewidth=1, c="red")
+    axs[2].axhline(y=y_max_line_placement_3, linewidth=1, c="red")
+
+    # Plot max error text
+    y_max_text_placement_1 = y_max_text_placement(max_error_angle1, RMSE_angle1)
+    y_max_text_placement_2 = y_max_text_placement(max_error_angle2, RMSE_angle2)
+    y_max_text_placement_3 = y_max_text_placement(max_error_angle3, RMSE_angle3)
+    axs[0].text(time1[-1] + 0.1 * (end_time - start_time), y_max_text_placement_1,
+                "Max = " + str(round(max_error_angle1, 1)) + " deg")
+    axs[1].text(time1[-1] + 0.1 * (end_time - start_time), y_max_text_placement_2,
+                "Max = " + str(round(max_error_angle2, 1)) + " deg")
+    axs[2].text(time1[-1] + 0.1 * (end_time - start_time), y_max_text_placement_3,
+                "Max = " + str(round(max_error_angle3, 1)) + " deg")
+
+    # Set a shared x axis
+    y_lim_list = np.array([max_error_angle1, max_error_angle2, max_error_angle3])
+    for i in range(0, 3):
+        axs[i].set(xlabel="Time [s]", ylabel="IMU Error [deg]", ylim=(0, 1.1 * y_lim_list[i]))
+        axs[i].grid(color="lightgrey")
+
+    fig.tight_layout(pad=2.0)
+
+    fig.savefig(figure_results_dir + r"\IMU_Orientation_Diff.png")
+
+    return RMSE_angle1, RMSE_angle2, RMSE_angle3
