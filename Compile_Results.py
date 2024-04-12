2fcba8c1c6b14d345c6a559e707e5e67d16d0fd4
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2024-04-12 14:52:44 +0100

    Updated Compile_Results.py to include IMU_type.

--- Compile_Results.py
+++ Compile_Results.py
@@ -6,13 +6,18 @@ import os
 # Quick Settings
 directory = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection'
 list_of_subjects = ['P1', 'P2', 'P3']
+IMU_type_list = ['Perfect', 'Real']
 trial_name = 'JA_Slow'
+# calibration_name_dict = {'ALL_MANUAL': None,
+#                          'Thorax_POSE_Rest_MANUAL': None,
+#                          'METHOD_1': None,
+#                          'METHOD_2_Alt_self': None,
+#                          'OSIM': None,
+#                          'OSIM_Alt_self': None}
 calibration_name_dict = {'ALL_MANUAL': None,
-                         'Thorax_POSE_Rest_MANUAL': None,
-                         'METHOD_1': None,
-                         'METHOD_2': None,
-                         'OSIM': None,
-                         'OSIM_Alt': None}
+                         'METHOD_2_Alt_self': None,
+                         'OSIM_N_self': None,
+                         'OSIM_Alt_self': None}
 
 # Function to read the results form csv
 def read_from_csv(file_path):
@@ -29,26 +34,29 @@ all_data = pd.DataFrame(columns=['Subject', 'Trial', 'CalibrationName', 'JA', 'R
 
 
 # Get the results for each calibration method
-for calibration_name in calibration_name_dict:
+for IMU_type in IMU_type_list:
 
-    for subject_code in list_of_subjects:
+    for calibration_name in calibration_name_dict:
 
-        # Define the file paths
-        comparison_folder = 'Comparison_' + subject_code + '_' + calibration_name + '_' + trial_name
-        file_name = subject_code + '_' + calibration_name + '_' + trial_name + '_Final_RMSEs.csv'
-        file_path = os.path.join(directory, subject_code, 'IMU_IK_results_' + calibration_name, trial_name,
-                                 comparison_folder, file_name)
+        for subject_code in list_of_subjects:
 
-        # Read RMSEs from the CSV file
-        with open(file_path, 'r') as file:
-            Results_df = pd.read_csv(file, index_col=0, header=0, sep=',', dtype={1: np.float64, 2: np.float64})
+            # Define the file paths
+            comparison_folder = 'Comparison_' + subject_code + '_' + calibration_name + '_' + trial_name
+            file_name = subject_code + '_' + calibration_name + '_' + trial_name + '_Final_RMSEs.csv'
+            file_path = os.path.join(directory, subject_code, IMU_type, 'IMU_IK_results_' + calibration_name, trial_name,
+                                     comparison_folder, file_name)
 
-        for row in range(len(Results_df)):
-            new_row = pd.DataFrame({'Subject': [subject_code], 'Trial': [trial_name],
-                                    'CalibrationName': [calibration_name], 'JA': Results_df.index[row],
-                                    'RMSE': Results_df.loc[Results_df.index[row], 'RMSE'],
-                                    'R':Results_df.loc[Results_df.index[row], 'R']})
-            all_data = pd.concat([all_data, new_row], ignore_index=True)
+            # Read RMSEs from the CSV file
+            with open(file_path, 'r') as file:
+                Results_df = pd.read_csv(file, index_col=0, header=0, sep=',', dtype={1: np.float64, 2: np.float64})
+
+            for row in range(len(Results_df)):
+                new_row = pd.DataFrame({'Subject': [subject_code], 'Trial': [trial_name],
+                                        'CalibrationName': [calibration_name], 'IMU_type': [IMU_type],
+                                        'JA': Results_df.index[row],
+                                        'RMSE': Results_df.loc[Results_df.index[row], 'RMSE'],
+                                        'R':Results_df.loc[Results_df.index[row], 'R']})
+                all_data = pd.concat([all_data, new_row], ignore_index=True)
 
 all_data.to_csv(os.path.join(directory, 'R Analysis', 'AllResults_forR.csv'))
 
