e3d3ab68d6236a5d7462004087e6059976ab9e9d
mmcinnes123 <m.mcinnnes1.22@abdn.ac.uk>
2024-05-23 18:35:53 +0100

    Wrote some functions for reading a JA/coords from a certain file based on pose time.

--- pose_analysis.py
+++ pose_analysis.py
@@ -1,24 +1,53 @@
 # This script analyses how successfully a subject performed each pose, relative to the 'perfect' default pose of the model
-
+import pandas as pd
 
 # Imports
 from pose_analysis_helpers import get_trial_pose_time_dict_from_file
+from pose_analysis_helpers import get_coords_from_mot
+from pose_analysis_helpers import get_coord_at_time_t
 
-import os
+from os.path import join
 
 """ SETTINGS """
-# subject_code_list = ['P1']
-subject_code = 'P1'
+subject_code_list = ['P1']
 
-# Define some file paths
-parent_dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection' + '\\' + subject_code
-OMC_dir = os.path.join(parent_dir, 'OMC')
 
-# Get the dict which defines when each pose happened in each trial
-trial_pose_time_dict = get_trial_pose_time_dict_from_file(parent_dir, subject_code)
 
-print(trial_pose_time_dict)
+
 """ MAIN """
+
+for subject_code in subject_code_list:
+
+    # Define some file paths
+    parent_dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection' + '\\' + subject_code
+    OMC_dir = join(parent_dir, 'OMC')
+
+    # Get the dict which defines when each pose happened in each trial
+    trial_pose_time_dict = get_trial_pose_time_dict_from_file(parent_dir, subject_code)
+
+    for trial_name in trial_pose_time_dict:
+
+        pose_time_dict = trial_pose_time_dict[trial_name]
+        for pose_name, pose_time in pose_time_dict.items():
+
+            # Find the directory with the IK results
+            IK_results_dir = join(OMC_dir, trial_name + '_IK_Results')
+            mot_file = join(IK_results_dir, 'OMC_IK_results.mot')
+
+            # Read in the .mot coords file
+            coords_table = get_coords_from_mot(mot_file)
+
+            # Get the coords (elbow flexion, pronation) at the specified pose time
+            elbow_flexion = get_coord_at_time_t(coords_table, pose_time, key='EL_x')
+            elbow_pronation = get_coord_at_time_t(coords_table, pose_time, key='PS_y')
+
+            # Or make this a dict, depending on what you do with it
+            df_row = pd.DataFrame({'Trial': [trial_name], 'Pose': [pose_name],
+                                    'elbow_flexion': [elbow_flexion], 'elbow_pronation': [elbow_pronation]})
+
+            # TODO: get the HT angles, using compare as a guide
+
+
 
 # Get all JAs from OMC IK results
 
--- pose_analysis_helpers.py
+++ pose_analysis_helpers.py
@@ -1,6 +1,7 @@
 # All the functions needed to run pose_analysis.py
 
 import ast
+import opensim as osim
 
 def get_trial_pose_time_dict_from_file(parent_dir, subject_code):
     # Read the existing txt file to get the dict
@@ -9,3 +10,20 @@ def get_trial_pose_time_dict_from_file(parent_dir, subject_code):
     trial_pose_time_dict = ast.literal_eval(content)
     file_obj.close()
     return trial_pose_time_dict
+
+
+def get_coords_from_mot(mot_file):
+
+    # Read in coordinates from IK results .mot files
+    print('Reading coordinates from .mot files...')
+    coords_table = osim.TimeSeriesTable(mot_file)
+
+    return coords_table
+
+def get_coord_at_time_t(coords_table, pose_time, key):
+
+    index = coords_table.getNearestRowIndexForTime(pose_time, restrictToTimeRange=True)
+    coord_column = coords_table.getDependentColumn(key)
+    coord = coord_column.row(index).to_numpy()
+
+    return coord
