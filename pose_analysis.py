e779828633c751e7b249613b83ca461ae8c30943
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2024-05-27 18:40:53 +0100

    Tidied function structure of pose_analysis.py.

--- pose_analysis.py
+++ pose_analysis.py
@@ -6,6 +6,8 @@ from pose_analysis_helpers import get_trial_pose_time_dict_from_file
 from pose_analysis_helpers import get_coords_from_mot
 from pose_analysis_helpers import get_coord_at_time_t
 from pose_analysis_helpers import get_HT_angles_from_sto
+from pose_analysis_helpers import get_coords_from_mot_file
+from pose_analysis_helpers import get_default_model_pose_dict
 
 from os.path import join
 import numpy as np
@@ -18,7 +20,8 @@ subject_code_list = ['P1']
 
 """ MAIN """
 
-all_results = pd.DataFrame(columns=['Trial', 'Pose', 'elbow_flexion', 'elbow_pronation', 'HT_abd', 'HT_flex', 'HT_rot'])
+all_results = pd.DataFrame(columns=['Trial', 'Pose', 'Pose_time', 'elbow_flexion', 'elbow_pronation', 'HT_abd', 'HT_flex', 'HT_rot',
+                                    'elbow_flexion_error', 'elbow_pronation_error', 'HT_abd_error', 'HT_flex_error', 'HT_rot_error'])
 
 for subject_code in subject_code_list:
 
@@ -36,70 +39,35 @@ for subject_code in subject_code_list:
 
         for pose_name, pose_time in pose_time_dict.items():
 
-            # Find the directory with the IK results
+            # Find the directory with the IK results files
             IK_results_dir = join(OMC_dir, trial_name + '_IK_Results')
             mot_file = join(IK_results_dir, 'OMC_IK_results.mot')
             analysis_sto_file = join(IK_results_dir, 'analyze_BodyKinematics_pos_global.sto')
 
-            # Read in the .mot coords file
-            coords_table = get_coords_from_mot(mot_file)
-
-            # Get the coords (elbow flexion, pronation) at the specified pose time
-            elbow_flexion = get_coord_at_time_t(coords_table, pose_time, key='EL_x')
-            elbow_pronation = get_coord_at_time_t(coords_table, pose_time, key='PS_y')
+            # Get some joint angles/coords from the mot file
+            elbow_flexion, elbow_pronation = get_coords_from_mot_file(mot_file, pose_time)
 
             # Get the values for HT joint angles from the sto file
             HT_abd, HT_flex, HT_rot = get_HT_angles_from_sto(analysis_sto_file, pose_time)
 
             # Compile the pose JAs into one dict
-            pose_dict = {'elbow_flexion': [elbow_flexion],
-                         'elbow_pronation': [elbow_pronation],
-                         'HT_abd': [HT_abd],
-                         'HT_flex': [HT_flex],
-                         'HT_rot': [HT_rot]}
+            pose_dict = {'elbow_flexion': elbow_flexion,
+                         'elbow_pronation': elbow_pronation,
+                         'HT_abd': HT_abd,
+                         'HT_flex': HT_flex,
+                         'HT_rot': HT_rot}
 
-            # Specify the default model pose Jas
-            if pose_name in ['N_self', 'N_asst']:
-                model_pose_dict = {'elbow_flexion': np.array([0]),
-                                   'elbow_pronation': np.array([90]),
-                                   'HT_abd': np.array([0]),
-                                   'HT_flex': np.array([0]),
-                                   'HT_rot': np.array([90])}
+            # Get the default model pose JAs, based on the pose_name
+            model_pose_dict = get_default_model_pose_dict(pose_name)
 
-            elif pose_name in ['Alt_self', 'Alt_asst']:
-                model_pose_dict = {'elbow_flexion': np.array([90]),
-                                   'elbow_pronation': np.array([90]),
-                                   'HT_abd': np.array([0]),
-                                   'HT_flex': np.array([0]),
-                                   'HT_rot': np.array([90])}
-
-            elif pose_name == 'Alt2_self':
-                model_pose_dict = {'elbow_flexion': np.array([90]),
-                                   'elbow_pronation': np.array([90]),
-                                   'HT_abd': np.array([0]),
-                                   'HT_flex': np.array([90]),
-                                   'HT_rot': np.array([90])}
-            else:
-                model_pose_dict = None
-                print('Pose name does not match one listed here.')
-
-
-
-            # Find the pose error in each JA
-            error_dict = {'Trial': [trial_name],
-                         'Pose': [pose_name],
-                         'elbow_flexion': None,
-                         'elbow_pronation': None,
-                         'HT_abd': None,
-                         'HT_flex': None,
-                         'HT_rot': None}
-
-            # Get the error (difference between performed pose, and model default pose) for each JA
-            for JA in model_pose_dict:
-                error_dict[JA] = model_pose_dict[JA][0] - pose_dict[JA][0]
+            # Find the pose error (difference between performed pose, and model default pose) for each JA
+            error_dict = {JA + '_error': pose_dict[JA][0] - model_pose_dict[JA][0] for JA in model_pose_dict}
 
             # Add the results from this pose into the results dataframe
-            new_df_row = pd.DataFrame(error_dict)
+            meta_dict = {'Trial': [trial_name],
+                         'Pose': [pose_name],
+                         'Pose_time': [pose_time]}
+            new_df_row = pd.DataFrame({**meta_dict, **pose_dict, **error_dict})
             all_results = pd.concat([all_results, new_df_row], ignore_index=True)
 
 
--- pose_analysis_helpers.py
+++ pose_analysis_helpers.py
@@ -7,6 +7,8 @@ import opensim as osim
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+
+# Function to open the .txt file which contains a dict with info on times, and pose names, for each trial
 def get_trial_pose_time_dict_from_file(parent_dir, subject_code):
     # Read the existing txt file to get the dict
     file_obj = open(parent_dir + '\\' + subject_code + '_cal_pose_dict.txt', 'r')
@@ -16,14 +18,21 @@ def get_trial_pose_time_dict_from_file(parent_dir, subject_code):
     return trial_pose_time_dict
 
 
-def get_coords_from_mot(mot_file):
+# Function to read an .mot file and get the relevant coordinates (joint angles) at a specific time
+def get_coords_from_mot_file(mot_file, pose_time):
 
     # Read in coordinates from IK results .mot files
-    print('Reading coordinates from .mot files...')
+    print(f'Reading coordinates from {mot_file} file...')
     coords_table = osim.TimeSeriesTable(mot_file)
 
-    return coords_table
+    # Get the coords (elbow flexion, pronation) at the specified pose time
+    elbow_flexion = get_coord_at_time_t(coords_table, pose_time, key='EL_x')
+    elbow_pronation = get_coord_at_time_t(coords_table, pose_time, key='PS_y')
 
+    return elbow_flexion, elbow_pronation
+
+
+# Function to get the coordinate at a specific time in the coords table
 def get_coord_at_time_t(coords_table, pose_time, key):
 
     index = coords_table.getNearestRowIndexForTime(pose_time, restrictToTimeRange=True)
@@ -33,25 +42,19 @@ def get_coord_at_time_t(coords_table, pose_time, key):
     return coord
 
 
-def get_HT_JAs():
-    thorax_IMU, humerus_IMU, radius_IMU = get_body_quats_from_analysis_sto(IMU_analysis_sto_path, start_time, end_time)
+# Function to get the humero-thoracic joint angles from an .sto file, using the model body oris to calculate projected vectors
+def get_HT_angles_from_sto(analysis_sto_file, pose_time):
+
+    # Read the sto file and get the orientation of each body at pose_time (in quats)
+    thorax_quats, humerus_quats, radius_quats = get_body_quats_from_analysis_sto(analysis_sto_file, pose_time)
+
+    # Get the projected vector angles from the body orientation data
+    abduction, flexion, rotation_elbow_down, rotation_elbow_up = get_vec_angles_from_two_CFs(thorax_quats, humerus_quats)
+
+    return abduction, flexion, rotation_elbow_down
 
 
-def get_body_ori_from_table(table, pose_time, key1, key2, key3):
-
-    # Get the body ori at pose_time
-    index = table.getNearestRowIndexForTime(pose_time, restrictToTimeRange=True)
-    ori1_column = table.getDependentColumn(key1)
-    ori1 = ori1_column.row(index).to_numpy()
-    ori2_column = table.getDependentColumn(key2)
-    ori2 = ori2_column.row(index).to_numpy()
-    ori3_column = table.getDependentColumn(key3)
-    ori3 = ori3_column.row(index).to_numpy()
-    eulers = np.stack((ori1, ori2, ori3), axis=1)
-
-    return eulers
-
-
+# Function to get the orientation (in quaternions) of each model body from the sto file
 def get_body_quats_from_analysis_sto(analysis_sto_path, pose_time):
 
     # Read in the analysis .sto file with pos and ori data for each model body
@@ -73,13 +76,54 @@ def get_body_quats_from_analysis_sto(analysis_sto_path, pose_time):
     return thorax_quats, humerus_quats, radius_quats
 
 
+# Function to get the body orientation from the data table, based on a given time
+def get_body_ori_from_table(table, pose_time, key1, key2, key3):
+
+    # Get the body ori at pose_time
+    index = table.getNearestRowIndexForTime(pose_time, restrictToTimeRange=True)
+    ori1_column = table.getDependentColumn(key1)
+    ori1 = ori1_column.row(index).to_numpy()
+    ori2_column = table.getDependentColumn(key2)
+    ori2 = ori2_column.row(index).to_numpy()
+    ori3_column = table.getDependentColumn(key3)
+    ori3 = ori3_column.row(index).to_numpy()
+    eulers = np.stack((ori1, ori2, ori3), axis=1)
+
+    return eulers
 
 
-def get_HT_angles_from_sto(analysis_sto_file, pose_time):
+# Function to get the JAs of the default model pose, based on the pose name
+def get_default_model_pose_dict(pose_name):
 
-    # Read the sto file and get the orientation of each body at pose_time (in quats)
-    thorax_quats, humerus_quats, radius_quats = get_body_quats_from_analysis_sto(analysis_sto_file, pose_time)
+    if pose_name in ['N_self', 'N_asst']:
+        model_pose_dict = {'elbow_flexion': np.array([0]),
+                           'elbow_pronation': np.array([90]),
+                           'HT_abd': np.array([0]),
+                           'HT_flex': np.array([0]),
+                           'HT_rot': np.array([90])}
 
-    abduction, flexion, rotation_elbow_down, rotation_elbow_up = get_vec_angles_from_two_CFs(thorax_quats, humerus_quats)
+    elif pose_name in ['Alt_self', 'Alt_asst']:
+        model_pose_dict = {'elbow_flexion': np.array([90]),
+                           'elbow_pronation': np.array([90]),
+                           'HT_abd': np.array([0]),
+                           'HT_flex': np.array([0]),
+                           'HT_rot': np.array([90])}
 
-    return abduction, flexion, rotation_elbow_down
+    elif pose_name == 'Alt2_self':
+        model_pose_dict = {'elbow_flexion': np.array([90]),
+                           'elbow_pronation': np.array([90]),
+                           'HT_abd': np.array([0]),
+                           'HT_flex': np.array([90]),
+                           'HT_rot': np.array([90])}
+    else:
+        model_pose_dict = None
+        print('Pose name does not match one listed here.')
+
+    return model_pose_dict
+
+
+
+
+
+
+
