437cfedce3725a5dfb2f1abed259c6e2ac1a6c14
mmcinnes123 <m.mcinnes1.22@abdn.ac.uk>
2024-06-26 10:46:37 +0100

    Added ability to auto read .txt files with event dicts. .

--- 2DoF_Axis_Est/opt_validation.py
+++ 2DoF_Axis_Est/opt_validation.py
@@ -4,6 +4,7 @@ from helpers_2DoF import get_J1_J2_from_calibrated_OMC_model
 from helpers_2DoF import get_J1_J2_from_opt
 from helpers_2DoF import get_J1_J2_from_isolate_move
 from helpers_2DoF import visulalise_3D_vec_on_IMU
+from helpers_2DoF import get_event_dict_from_file
 
 import qmt
 import opensim as osim
@@ -13,12 +14,10 @@ from os.path import join
 import logging
 import numpy as np
 from tkinter.filedialog import askopenfilename, askdirectory
-np.set_printoptions(suppress=True)
 
+np.set_printoptions(suppress=True)
 osim.Logger.setLevelString("Off")
 logging.basicConfig(level=logging.INFO, filename="FE_axis.log", filemode="w")
-
-
 
 
 """" RUN FUNCTIONS ABOVE """
@@ -28,32 +27,27 @@ sample_rate = 100          # This is the sample rate of the data going into the 
 trial_for_opt = 'JA_Slow'
 IMU_type_for_opt = 'Perfect'
 opt_method = 'rot'
-time_dict = {
-    'P1': {'JA_Slow': {'FE_start': 16, 'FE_end': 29, 'PS_start': 31, 'PS_end': 47}},
-    'P2': {'JA_Slow': {'FE_start': 15, 'FE_end': 28, 'PS_start': 30, 'PS_end': 46}},
-    'P3': {'JA_Slow': {'FE_start': 18, 'FE_end': 34, 'PS_start': 35, 'PS_end': 49}},
-    'P4': {'JA_Slow': {'FE_start': 23, 'FE_end': 39, 'PS_start': 42, 'PS_end': 60}},
-    'P5': {'JA_Slow': {'FE_start': 30, 'FE_end': 47, 'PS_start': 48, 'PS_end': 65}},
-    'P6': {'JA_Slow': {'FE_start': 26, 'FE_end': 44, 'PS_start': 47, 'PS_end': 65}}
-             }
-
-# subject_list = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6']
-subject_list = ['P1', 'P2']
-# subject_list = []
-
 logging.info(f'Using IMU type: {IMU_type_for_opt}, with data from trial: {trial_for_opt}.')
 
+# List of subjects
+# subject_list = [f'P{i}' for i in range(1, 23)]
+subject_list = ['P13']
+
+# Initiate dict to store the calculated error for each subject
 opt_rel2_OMC_errors = {}
 
 for subject_code in subject_list:
 
     logging.info(f'Results for Subject {subject_code}')
+    print(f'Running analysis for {subject_code}.')
 
     # Define some files
     parent_dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection' + '\\' + subject_code
     OMC_dir = join(parent_dir, 'OMC')
     model_file = join(OMC_dir, 'das3_scaled_and_placed.osim')
-    subject_time_dict = time_dict[subject_code]
+
+    # Get the dict with the timings for FE and PS events
+    subject_event_dict = get_event_dict_from_file(subject_code)
 
     """ FINDING REFERENCE J1 AXIS IN HUMERUS CLUSTER FRAME """
     OMC_FE = get_J1_J2_from_calibrated_OMC_model(model_file, debug=False)
@@ -61,7 +55,7 @@ for subject_code in subject_list:
 
     """ FINDING FE AND PS FROM OPTIMISATION RESULT """
     opt_FE, opt_PS, opt_results = get_J1_J2_from_opt(subject_code, IMU_type_for_opt, trial_for_opt,
-                                                     opt_method, subject_time_dict, sample_rate, debug=False)
+                                                     opt_method, subject_event_dict, sample_rate, debug=True)
     heading_offset = opt_results['delta']*180/np.pi
     logging.info(f'Opt (rot) FE axis in humerus IMU frame: {opt_FE}')
     logging.info(f'Opt (rot) PS axis in forearm IMU frame: {opt_PS}')
@@ -88,12 +82,15 @@ for subject_code in subject_list:
 
     opt_rel2_OMC_errors[subject_code] = opt_error
 
+    print(f'Error: {opt_error}')
+    print(f'Finished analysis for {subject_code}.')
+
 
 """ COMPILE ALL RESULTS """
 
 
 all_errors = np.array(list(opt_rel2_OMC_errors.values()))
-mean_all_errors = np.mean(all_errors)
+mean_all_errors = np.nanmean(all_errors)
 
 logging.info(f'All errors: {opt_rel2_OMC_errors}.')
 logging.info(f'Mean: {mean_all_errors}.')
