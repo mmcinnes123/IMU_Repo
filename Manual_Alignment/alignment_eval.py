26f00fe7d483d9ebdc0f52f6aba57c0b28a728ba
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2024-08-05 18:26:32 +0100

    Finished alignment_eval.py code to plot and get error values.

--- Manual_Alignment/alignment_eval.py
+++ Manual_Alignment/alignment_eval.py
@@ -1,177 +1,75 @@
 # This script analyses how well the manual placement of the sensors matches the CFs of the underlying body segments
 # Using perfect IMU data (i.e. marker cluster CFs) and calibrated OMC model
+import pandas as pd
 import qmt
 
-from alignment_eval_helpers import get_IMU_in_body_frame
+from alignment_eval_helpers import get_all_clus_in_body_frame
+from alignment_eval_helpers import plot_local_vec_on_global_plane
 
-from os.path import join
 import opensim as osim
+from os.path import join
 
-import plotly.figure_factory as ff
-import plotly.graph_objects as go
-import plotly
-import numpy as np
-
-
-dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection'
 
 # Repress opensim logging
 osim.Logger.setLevelString("Off")
 
-
-
-def get_all_clus_in_body_frame(subject_list):
-
-    # Initiate empty list with hum_cluster ori results
-    hum_clus_all = {}
-    rad_clus_all = {}
-
-    for subject_code in subject_list:
-
-        OMC_dir = join(dir, subject_code, 'OMC')
-        model_file = join(OMC_dir, 'das3_scaled_and_placed.osim')
-
-        # Get the orientation of the 'perfect' IMU CF (cluster CF), in the body CF
-        hum_clus_in_hum, rad_clus_in_rad = get_IMU_in_body_frame(model_file)
-
-        hum_clus_all[subject_code] = hum_clus_in_hum
-        rad_clus_all[subject_code] = rad_clus_in_rad
-
-    return hum_clus_all, rad_clus_all
-
-
-def plot_local_vec_on_global_plane(clus_all, JA_name, local_axis, global_axis_1, global_axis_2):
-
-    # Initiate dict with vecs
-    vecs_dict = {}
-
-    for subject in clus_all.keys():
-
-        hum_clus_in_hum = clus_all[subject]
-
-        local_index = dict(x=0, y=1, z=2)[local_axis]
-        global_index_1 = dict(X=0, Y=1, Z=2)[global_axis_1]
-        global_index_2 = dict(X=0, Y=1, Z=2)[global_axis_2]
-
-        # Get the local IMU CF axes of interest
-        hum_clus_local_vec = qmt.quatToRotMat(hum_clus_in_hum)[:, local_index]
-
-        # Get the local axis vector on the 2D plane of interest
-        hum_clus_vec_on_2D_plane = [hum_clus_local_vec[global_index_1], hum_clus_local_vec[global_index_2]]
-
-        vecs_dict[subject] = hum_clus_vec_on_2D_plane
-
-
-    # Define the CF axes
-    axis_u = [0, 1]
-    axis_v = [1, 0]
-    o_u = [0]
-    o_v = [0]
-    v_axis_label = global_axis_1
-    u_axis_label = global_axis_2
-
-    vecs = np.array(list(vecs_dict.values()))
-    vec_us = vecs[:, 0]
-    vec_vs = vecs[:, 1]
-    vec_origins_u = [0]*len(vec_us)
-    vec_origins_v = [0]*len(vec_us)
-
-
-    # Create the first quiver plot (red)
-    quiver_vecs = ff.create_quiver(vec_origins_u, vec_origins_v, vec_us, vec_vs, line_color='red', scale=1, arrow_scale=.1, angle=np.pi / 6,
-                                   name='vec1', line=dict(width=1))
-
-    # Create the second quiver plot (blue)
-    quiver_U = ff.create_quiver(o_u, o_v, [axis_u[0]], [axis_u[1]], line_color='black', scale=1, arrow_scale=.1, angle=np.pi / 6,
-                                name='vec1', line=dict(width=1))
-    quiver_V = ff.create_quiver(o_u, o_v, [axis_v[0]], [axis_v[1]], line_color='black', scale=1, arrow_scale=.1, angle=np.pi / 6,
-                                name='vec1', line=dict(width=1))
-
-
-    # Initialize the figure
-    fig = go.Figure()
-
-    # Add the first quiver plot
-    for trace in quiver_vecs['data']:
-        fig.add_trace(trace)
-
-    # Add the second quiver plot
-    for trace in quiver_U['data']:
-        fig.add_trace(trace)
-
-    # Add the third quiver plot
-    for trace in quiver_V['data']:
-        fig.add_trace(trace)
-
-    # Add annotations with custom labels
-    annotations = [
-        # dict(
-        #     x=vecs[0], y=vecs[1], xref="x", yref="y",
-        #     text=vec_label, showarrow=False,
-        #     xanchor="left", yanchor="top",  # Offset position
-        #     xshift=10, yshift=10  # Pixel offset from the point
-        # ),
-        dict(
-            x=axis_u[0], y=axis_u[1], xref="x", yref="y",
-            text=u_axis_label, showarrow=False, xanchor="center", yanchor="bottom",  # Offset position
-            xshift=0, yshift=0  # Pixel offset from the point
-        ),
-        dict(
-            x=axis_v[0], y=axis_v[1], xref="x", yref="y",
-            text=v_axis_label, showarrow=False, xanchor="right", yanchor="middle",  # Offset position
-            xshift=0, yshift=0  # Pixel offset from the point
-        )
-    ]
-
-
-
-    # Set the axes labels and properties
-    fig.update_layout(
-        xaxis=dict(
-            title='',
-            zeroline=True,
-            # autorange='reversed',  # Invert the x-axis
-            range = [2, -2],
-            showgrid=False,
-            showline=False,
-            showticklabels=False
-        ),
-        yaxis=dict(
-            title='',
-            zeroline=True,
-            scaleanchor="x",  # Ensure equal scaling
-            scaleratio=1,
-            showgrid=False,
-            showline=False,
-            showticklabels=False
-        ),
-        title={
-            'text': f'{JA_name}: IMU {local_axis}-axis on the {global_axis_1}{global_axis_2} Plane',
-            'x': 0.5,
-            'xanchor': 'center'},
-        showlegend=False,
-        width=600,  # Adjust width to ensure equal scaling visually
-        height=600,
-        plot_bgcolor='white',
-        annotations=annotations
-    )
-
-    # Display the plot
-    # fig.show()
-    save_file = join(r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection\R Analysis\Manual Alignment', JA_name + '.png')
-    fig.write_image(save_file)
-
-
+# Define the list of subjects
 subject_list = [f'P{str(i).zfill(3)}' for i in range(1, 21)]
+
+""" 
+Define the settings for each JA of interest.
+    local_axis - the axis of the IMU which we want to measure the direction of
+    global_axis_1 - one of two axis of the body coordinate frame which defines the plane of interest (points left)
+    global_axis_2 - one of two axis of the body coordinate frame which defines the plane of interest (points down)
+    target_global_axis - the axis of the body coordinate frame which should be well-aligned with the IMU local axis
+"""
+
+sh_flex_settings = dict(JA_name='Shoulder Flexion', local_axis='y', global_axis_1='Z', global_axis_2='Y', target_global_axis='-Y')
+sh_abd_settings = dict(JA_name='Shoulder Abduction', local_axis='y', global_axis_1='X', global_axis_2='Y', target_global_axis='-Y')
+sh_rot_settings = dict(JA_name='Shoulder Rotation', local_axis='z', global_axis_1='X', global_axis_2='Z', target_global_axis='X')
+el_flex_settings = dict(JA_name='Elbow Flexion', local_axis='y', global_axis_1='Y', global_axis_2='X', target_global_axis='-Y')
+el_abd_settings = dict(JA_name='Elbow Abduction', local_axis='z', global_axis_1='Z', global_axis_2='Y', target_global_axis='Z')
+pro_sup_settings = dict(JA_name='Pronation/Supination', local_axis='z', global_axis_1='Z', global_axis_2='X', target_global_axis='Z')
+
+
+""" 
+From the calibrated OMC models, get the orientation of the marker cluster frames (representing 'perfect' IMUs) 
+defined in the frame of the associated body (e.g. humerus or radius frame).
+"""
 
 hum_clus_all, rad_clus_all = get_all_clus_in_body_frame(subject_list)
 
-# Set the local axis to be the projected vector, and define the 2D plane with global_axis_1 (points left)
-# and global_axis_2 (points up)
-plot_local_vec_on_global_plane(hum_clus_all, JA_name='Shoulder Flexion', local_axis='y', global_axis_1='Z', global_axis_2='Y')
-# plot_local_vec_on_global_plane(hum_clus_all, JA_name='Shoulder Abduction', local_axis='y', global_axis_1='X', global_axis_2='Y')
-# plot_local_vec_on_global_plane(hum_clus_all, JA_name='Shoulder Rotation', local_axis='z', global_axis_1='X', global_axis_2='Z')
+""" 
+For each joint angle, for each subject, get the 2D vector projected onto the plane of interest, plot these on a 2D plot, 
+ and get the angular difference ('error') between the vector and the 'target vector', and the variation in that error. 
+ """
 
-# plot_local_vec_on_global_plane(rad_clus_all, JA_name='Elbow Flexion', local_axis='y', global_axis_1='Y', global_axis_2='X')
-# plot_local_vec_on_global_plane(rad_clus_all, JA_name='Elbow Abduction', local_axis='z', global_axis_1='Z', global_axis_2='Y')
-# plot_local_vec_on_global_plane(rad_clus_all, JA_name='Pronation/Supination', local_axis='z', global_axis_1='Z', global_axis_2='X')
+all_results = pd.DataFrame(columns=['JA', 'mean_error', 'SD_error'])
+
+for JA_settings in [sh_flex_settings, sh_abd_settings, sh_rot_settings]:
+    mean_error, SD_error = plot_local_vec_on_global_plane(hum_clus_all,
+                                   JA_name=JA_settings['JA_name'],
+                                   local_axis=JA_settings['local_axis'],
+                                   global_axis_1=JA_settings['global_axis_1'],
+                                   global_axis_2=JA_settings['global_axis_2'],
+                                   target_global_axis=JA_settings['target_global_axis'])
+    new_row = pd.DataFrame({'JA': [JA_settings['JA_name']], 'mean_error': [mean_error],
+                            'SD_error': [SD_error]})
+    # Log the results
+    all_results = pd.concat([all_results, new_row], ignore_index=True)
+
+for JA_settings in [el_flex_settings, el_abd_settings, pro_sup_settings]:
+    mean_error, SD_error = plot_local_vec_on_global_plane(rad_clus_all,
+                                   JA_name=JA_settings['JA_name'],
+                                   local_axis=JA_settings['local_axis'],
+                                   global_axis_1=JA_settings['global_axis_1'],
+                                   global_axis_2=JA_settings['global_axis_2'],
+                                   target_global_axis=JA_settings['target_global_axis'])
+    new_row = pd.DataFrame({'JA': [JA_settings['JA_name']], 'mean_error': [mean_error],
+                            'SD_error': [SD_error]})
+    # Log the results
+    all_results = pd.concat([all_results, new_row], ignore_index=True)
+
+# Save results to csv
+save_file_path = join(r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection\R Analysis\Manual Alignment', 'Alignment_results.csv')
+all_results.to_csv(save_file_path, index=False)
--- Manual_Alignment/alignment_eval_helpers.py
+++ Manual_Alignment/alignment_eval_helpers.py
@@ -1,6 +1,10 @@
 
 import opensim as osim
 import qmt
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import numpy as np
+from os.path import join
 
 
 
@@ -28,3 +32,194 @@ def get_IMU_in_body_frame(model_file):
     rad_clus_in_rad = qmt.quatFrom2Axes(rad_clus_x_axis, rad_clus_y_axis, None, plot=False)
 
     return hum_clus_in_hum, rad_clus_in_rad
+
+
+def get_all_clus_in_body_frame(subject_list):
+
+    parent_dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection'
+
+    # Initiate empty list with hum_cluster ori results
+    hum_clus_all = {}
+    rad_clus_all = {}
+
+    for subject_code in subject_list:
+
+        OMC_dir = join(parent_dir, subject_code, 'OMC')
+        model_file = join(OMC_dir, 'das3_scaled_and_placed.osim')
+
+        # Get the orientation of the 'perfect' IMU CF (cluster CF), in the body CF
+        hum_clus_in_hum, rad_clus_in_rad = get_IMU_in_body_frame(model_file)
+
+        hum_clus_all[subject_code] = hum_clus_in_hum
+        rad_clus_all[subject_code] = rad_clus_in_rad
+
+    return hum_clus_all, rad_clus_all
+
+
+def get_vecs_and_error_between_local_and_global(clus_all, JA_name, local_axis, global_axis_1, global_axis_2,
+                                                target_global_axis):
+    # Initiate dict with vecs
+    vecs_dict = {}
+    error_angles_dict = {}
+
+    for subject in clus_all.keys():
+
+        clus_in_body = clus_all[subject]
+
+        local_index = dict(x=0, y=1, z=2)[local_axis]
+        global_index_1 = dict(X=0, Y=1, Z=2)[global_axis_1]
+        global_index_2 = dict(X=0, Y=1, Z=2)[global_axis_2]
+
+        # Get the target global axis, expressed in the global plane
+        assert target_global_axis.lstrip('-') in [global_axis_1, global_axis_2]
+        if target_global_axis.lstrip('-') == global_axis_1:
+            target_vec = np.array([1, 0])
+        elif target_global_axis.lstrip('-') == global_axis_2:
+            target_vec = np.array([0, 1])
+        else:
+            target_vec = np.array([0, 0])
+            quit()
+
+        if target_global_axis.startswith('-'):
+            target_vec = -target_vec
+
+        # Get the local IMU CF axes of interest
+        clus_local_vec = qmt.quatToRotMat(clus_in_body)[:, local_index]
+
+        # Get the local axis vector on the 2D plane of interest
+        clus_vec_on_2D_plane = [clus_local_vec[global_index_1], clus_local_vec[global_index_2]]
+        vecs_dict[subject] = clus_vec_on_2D_plane
+
+        # Get the angle between the vector and the target vector (always positive)
+        angle_between = angle_between_two_2D_vecs(target_vec, clus_vec_on_2D_plane)
+        error_angles_dict[subject] = angle_between
+
+    return error_angles_dict, vecs_dict
+
+
+def get_variation_in_error_angles(error_angles_dict):
+
+    error_angles = np.array(list(error_angles_dict.values()))
+
+    error_angles_mean = np.mean(error_angles)
+
+    error_anlges_SD = np.std(error_angles)
+
+    return error_angles_mean, error_anlges_SD
+
+
+def plot_local_vec_on_global_plane(clus_all, JA_name, local_axis, global_axis_1, global_axis_2, target_global_axis):
+
+    error_angles_dict, vecs_dict = get_vecs_and_error_between_local_and_global(clus_all, JA_name,
+                                                                               local_axis, global_axis_1,
+                                                                               global_axis_2,
+                                                                               target_global_axis)
+    error_angles_mean, error_angles_SD = get_variation_in_error_angles(error_angles_dict)
+
+    # Define the CF axes
+    axis_u = [0, 1]
+    axis_v = [1, 0]
+    o_u = [0]
+    o_v = [0]
+    v_axis_label = global_axis_1
+    u_axis_label = global_axis_2
+
+    vecs = np.array(list(vecs_dict.values()))
+    vec_us = vecs[:, 0]
+    vec_vs = vecs[:, 1]
+    vec_origins_u = [0]*len(vec_us)
+    vec_origins_v = [0]*len(vec_us)
+
+
+    # Create the first quiver plot (red)
+    quiver_vecs = ff.create_quiver(vec_origins_u, vec_origins_v, vec_us, vec_vs, line_color='red', scale=1, arrow_scale=.1, angle=np.pi / 6,
+                                   name='vec1', line=dict(width=1))
+
+    # Create the second quiver plot (blue)
+    quiver_U = ff.create_quiver(o_u, o_v, [axis_u[0]], [axis_u[1]], line_color='black', scale=1, arrow_scale=.1, angle=np.pi / 6,
+                                name='vec1', line=dict(width=1))
+    quiver_V = ff.create_quiver(o_u, o_v, [axis_v[0]], [axis_v[1]], line_color='black', scale=1, arrow_scale=.1, angle=np.pi / 6,
+                                name='vec1', line=dict(width=1))
+
+
+    # Initialize the figure
+    fig = go.Figure()
+
+    # Add the first quiver plot
+    for trace in quiver_vecs['data']:
+        fig.add_trace(trace)
+
+    # Add the second quiver plot
+    for trace in quiver_U['data']:
+        fig.add_trace(trace)
+
+    # Add the third quiver plot
+    for trace in quiver_V['data']:
+        fig.add_trace(trace)
+
+    # Add annotations with custom labels
+    annotations = [
+        # dict(
+        #     x=vecs[0], y=vecs[1], xref="x", yref="y",
+        #     text=vec_label, showarrow=False,
+        #     xanchor="left", yanchor="top",  # Offset position
+        #     xshift=10, yshift=10  # Pixel offset from the point
+        # ),
+        dict(
+            x=axis_u[0], y=axis_u[1], xref="x", yref="y",
+            text=u_axis_label, showarrow=False, xanchor="center", yanchor="bottom",  # Offset position
+            xshift=0, yshift=0  # Pixel offset from the point
+        ),
+        dict(
+            x=axis_v[0], y=axis_v[1], xref="x", yref="y",
+            text=v_axis_label, showarrow=False, xanchor="right", yanchor="middle",  # Offset position
+            xshift=0, yshift=0  # Pixel offset from the point
+        )
+    ]
+
+
+
+    # Set the axes labels and properties
+    fig.update_layout(
+        xaxis=dict(
+            title='',
+            zeroline=True,
+            # autorange='reversed',  # Invert the x-axis
+            range = [2, -2],
+            showgrid=False,
+            showline=False,
+            showticklabels=False
+        ),
+        yaxis=dict(
+            title='',
+            zeroline=True,
+            scaleanchor="x",  # Ensure equal scaling
+            scaleratio=1,
+            showgrid=False,
+            showline=False,
+            showticklabels=False
+        ),
+        title={
+            'text': f'{JA_name}: IMU {local_axis}-axis on the {global_axis_1}{global_axis_2} Plane',
+            'x': 0.5,
+            'xanchor': 'center'},
+        showlegend=False,
+        width=600,  # Adjust width to ensure equal scaling visually
+        height=600,
+        plot_bgcolor='white',
+        annotations=annotations
+    )
+
+    # Display the plot
+    # fig.show()
+
+    return error_angles_mean, error_angles_SD
+
+def angle_between_two_2D_vecs(target_vec, vec2):
+
+    # Calculate the angle (always a positive result)
+    angle = np.arccos(np.dot(target_vec, vec2) / (np.linalg.norm(target_vec) * np.linalg.norm(vec2)))
+
+    angle_deg = np.rad2deg(angle)
+
+    return angle_deg
